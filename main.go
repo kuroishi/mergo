7226bf862b4ffad1662c640bd910b3d1211c055a
KUROISHI Mitsuo <kuroishi@iij.ad.jp>
2021-12-23 13:41:17 +0900

    update.

--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"strconv"
-	"bufio"
 	"sort"
+	"strconv"
 	"sync"
 )
 
 func dosort(l []int, r []int) (ret []int) {
 	llen := len(l)
 	rlen := len(r)
-	for i, lidx, ridx := 0, 0, 0; i < llen + rlen; i++ {
+	for i, lidx, ridx := 0, 0, 0; i < llen+rlen; i++ {
 		if lidx >= llen {
 			ret = append(ret, r[ridx])
 			ridx++
@@ -44,7 +44,7 @@ func gosort(nums []int) []int {
 }
 
 func smergesort(nums []int) []int {
-	var sorted [][]int
+	sorted := make([][]int, len(nums))
 	for _, v := range nums {
 		sorted = append(sorted, []int{v})
 	}
@@ -58,64 +58,64 @@ func smergesort(nums []int) []int {
 }
 
 func pmergesort(nums []int) []int {
-	var sorted [][]int
+	sorted := make([][]int, len(nums))
 	for _, v := range nums {
 		sorted = append(sorted, []int{v})
 	}
 
-	sortedl := sorted[:len(sorted) / 2]
-	sortedr := sorted[len(sorted) / 2:]
-	
-	sortedll := make([][]int, len(sortedl) / 2)
-	sortedlr := make([][]int, len(sortedl) - (len(sortedl) / 2))
-	sortedrl := make([][]int, len(sortedr) / 2)
-	sortedrr := make([][]int, len(sortedr) - (len(sortedr) / 2))
-	
-	copy(sortedll, sortedl[:len(sortedl) / 2])
-	copy(sortedlr, sortedl[len(sortedl) / 2:])
-	copy(sortedrl, sortedr[:len(sortedr) / 2])
-	copy(sortedrr, sortedr[len(sortedr) / 2:])
+	sortedl := sorted[:len(sorted)/2]
+	sortedr := sorted[len(sorted)/2:]
+
+	sortedll := make([][]int, len(sortedl)/2)
+	sortedlr := make([][]int, len(sortedl)-(len(sortedl)/2))
+	sortedrl := make([][]int, len(sortedr)/2)
+	sortedrr := make([][]int, len(sortedr)-(len(sortedr)/2))
+
+	copy(sortedll, sortedl[:len(sortedl)/2])
+	copy(sortedlr, sortedl[len(sortedl)/2:])
+	copy(sortedrl, sortedr[:len(sortedr)/2])
+	copy(sortedrr, sortedr[len(sortedr)/2:])
 
 	var sortedall [][][]int
 	sortedall = append(sortedall, sortedll)
 	sortedall = append(sortedall, sortedlr)
 	sortedall = append(sortedall, sortedrl)
-	sortedall = append(sortedall, sortedrr)	
+	sortedall = append(sortedall, sortedrr)
 	//fmt.Println(sortedall[0])
 	//fmt.Println(sortedall[1])
 	//fmt.Println(sortedall[2])
-	//fmt.Println(sortedall[3])	
+	//fmt.Println(sortedall[3])
 
 	var wg sync.WaitGroup
 	for idx := 0; idx < 4; idx++ {
 		wg.Add(1)
-                go func(idx int) {
-		    defer wg.Done()
-                    for len(sortedall[idx]) != 1 {
-                        //fmt.Printf("sortedall[%d]: %v len: %d\n", idx, sortedall[idx], len(sortedall[idx]))
-                        sortedall[idx] = append(sortedall[idx], dosort(sortedall[idx][0], sortedall[idx][1]))
-                        sortedall[idx] = sortedall[idx][2:]
-                    }
-                }(idx)
+		go func(idx int) {
+			defer wg.Done()
+			for len(sortedall[idx]) != 1 {
+				//fmt.Printf("sortedall[%d]: %v len: %d\n", idx, sortedall[idx], len(sortedall[idx]))
+				sortedall[idx] = append(sortedall[idx], dosort(sortedall[idx][0], sortedall[idx][1]))
+				sortedall[idx] = sortedall[idx][2:]
+			}
+		}(idx)
 	}
 	wg.Wait()
 
-       	chl := make(chan []int)
+	chl := make(chan []int)
 	go func(ch chan []int) {
 		ch <- dosort(sortedall[0][0], sortedall[1][0])
- 
+
 	}(chl)
 
-       	chr := make(chan []int)
+	chr := make(chan []int)
 	go func(ch chan []int) {
-		ch <- dosort(sortedall[2][0], sortedall[3][0])	
+		ch <- dosort(sortedall[2][0], sortedall[3][0])
 	}(chr)
-	
+
 	return dosort(<-chl, <-chr)
 }
 
 func main() {
-	var nums []int	
+	var nums []int
 	sc := bufio.NewScanner(os.Stdin)
 	for sc.Scan() {
 		n, _ := strconv.Atoi(sc.Text())
@@ -125,7 +125,7 @@ func main() {
 	//ret := gosort(nums)
 	//ret := smergesort(nums)
 	ret := pmergesort(nums)
-	
+
 	for idx, _ := range ret {
 		fmt.Print(ret[idx], "\n")
 	}
